1fbbefab980dbaeac7615286d22593135b61483c
tetris11 <tetris11>
2016-11-16 15:21:27 +0000

    initial again

---
+++ initial_build.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+ref=main
+browse=chromium
+
+# when in doubt, throw hammers...
+(
+    pdflatex -synctex=1 -interaction=nonstopmode $ref.tex
+    bibtex $ref.aux
+    bibtex $ref.aux
+    pdflatex -synctex=1 -interaction=nonstopmode $ref.tex
+    pdflatex -synctex=1 -interaction=nonstopmode $ref.tex
+
+) && $browse $ref.pdf
+
+
+#ps aux \
+# | grep -P "^(pdf|chrom|fire)"\
+# | awk -F '\t' '{print $2"\t"$NF}'\
+# | sed 's/chrom/fire/'\
+# 2> /dev/null > /tmp/out \
+# && cat /tmp/out\
+#     | cut -f 2\
+#     | 
+ 
