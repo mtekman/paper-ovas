99c11b98f8c02f94cc30ddc23588411b3d14a115
tetris <tetris11>
2017-02-22 14:35:51 +0000

    simplified macros

--- initial_build.sh
+++ initial_build.sh
@@ -3,7 +3,6 @@
 ref=main
 browse=firefox
 
-# when in doubt, throw hammers...
 (
     pdflatex -synctex=1 -interaction=nonstopmode $ref.tex
     bibtex $ref.aux
@@ -12,14 +11,3 @@ browse=firefox
     pdflatex -synctex=1 -interaction=nonstopmode $ref.tex
 
 ) && $browse $ref.pdf
-
-
-#ps aux \
-# | grep -P "^(pdf|chrom|fire)"\
-# | awk -F '\t' '{print $2"\t"$NF}'\
-# | sed 's/chrom/fire/'\
-# 2> /dev/null > /tmp/out \
-# && cat /tmp/out\
-#     | cut -f 2\
-#     | 
- 
