fca4bb51ee2c27b73db3cf564ce0af8607fee15c
Mehmet Tekman <m.tekman@ucl.ac.uk>
2017-08-13 22:48:35 +0100

    new

--- bmc_bioinformatics/process.sh
+++ bmc_bioinformatics/process.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-zip bmcbio_ovas_paper_2017_06_09.ver3.zip\
+dfmt=`date +%Y_%m_%d`
+
+zip bmcbio_ovas_paper_${dfmt}.zip\
     bmc_article.tex\
     fig{1,2,3,4}.png\
     bibliography.bib\
-    vancouver.bst\
+    spbasic.bst\
     bmcart.cls\
     bmcart-biblio.sty\
-    "Reference PDF".pdf
+    "Reference PDF".pdf\
+    Response-to-Reviewers.docx\
+    manuscript_with_highlighted_changes.pdf
